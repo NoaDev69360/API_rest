4ebaee0f8b3b49bb5747f62e6afb40ce84f2449b
Noa Ali-guechi <noaaliguechi@gmail.com>
2023-04-07 10:22:38 +0200

    lien entre l'api et la base de donnée

---
+++ vendor/composer/installed.php
@@ -0,0 +1,32 @@
+<?php return array(
+    'root' => array(
+        'name' => 'api-rest-noa/products-rest-api',
+        'pretty_version' => 'dev-master',
+        'version' => 'dev-master',
+        'reference' => '5afd8fba48e0fd47eaffeb28dbc33ac42f7ccf37',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        'api-rest-noa/products-rest-api' => array(
+            'pretty_version' => 'dev-master',
+            'version' => 'dev-master',
+            'reference' => '5afd8fba48e0fd47eaffeb28dbc33ac42f7ccf37',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+        'symfony/dotenv' => array(
+            'pretty_version' => 'v6.2.8',
+            'version' => '6.2.8.0',
+            'reference' => '4481aa45be7a11d2335c1d5b5bbe2f0c6199b105',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../symfony/dotenv',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
